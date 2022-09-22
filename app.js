7791ef9df57e11782923968de137400b60bbaa96
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2022-09-23 00:32:43 +0300

    Javascript Variables, Variables Identifiers

---
+++ app.js
@@ -0,0 +1,29 @@
+/*
+
+Javascript Variable novleri
+
+var 
+let
+const
+
+*/
+// var aziz = 500;
+// const mehemmed = 600;
+// aziz = aziz + 100;
+// console.log("Aziz emek haqqisi: " + aziz)
+// console.log("Mehemmed emek haqqisi: " + mehemmed)
+// var eli = 100;
+// var Eli = 200;
+// console.log(aziz)
+// console.log(Aziz)
+
+
+{
+    let aziz = 100;
+console.log(aziz)
+
+}
+
+
+
+
